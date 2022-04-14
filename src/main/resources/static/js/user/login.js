eb5934fbe759e34ce280e4c6636a657d3dd94b14
ranyiyan <101090809+ranyiyan@users.noreply.github.com>
2022-04-14 12:33:51 +0800

    Update login.js

--- src/main/resources/static/js/user/login.js
+++ src/main/resources/static/js/user/login.js
@@ -1,36 +1,50 @@
 $(function () {
-    // 给验证码的图片，绑定单击事件
+    // Bind the click event to the picture of the verification code
     $("#code_img").click(function () {
+        //There is a 'this' object in the 'function' function of the event response. This 'this' object is the dom object that is currently responding to the event
+        //SRC attribute indicates the picture path of verification code img label.It is readable and writable
+        //alert(this.src);
         this.src = "loginimage?d=" + Math.random();
     });
 
+    //Click event to register binding
     $("#signin").click(function () {
+        //Authentication username: it must be composed of letters, numbers and underscores, and the length is 5 to 12 digits
+        //1 get the content in the username input box
         var usernameText = $("#username").val();
+        //2 create regular expression object
         var usernamePatt = /^\w{5,12}$/;
         if (!usernamePatt.test(usernameText)) {
+            //3 prompt user for results
             $("span.errorMsg").text("Illegal Username!");
             return false;
         }
+
+
+        //Verification password: it must be composed of letters, numbers and underscores, and the length is 5 to 12 digits
+        //1 get the content in the username input box
         var passwordText = $("#password").val();
+        //2 create regular expression object
         var passwordPatt = /^\w{5,12}$/;
         if (!passwordPatt.test(passwordText)) {
+            //3 prompt user for results
             $("span.errorMsg").text("Illegal Password!");
             return false;
         }
 
         var codeText = $("#code").val();
 
-        //去掉验证码前后空格
-        // alert("去空格前：["+codeText+"]")
+        //Remove the space before and after the verification code
+        // alert("Before deleting the space：["+codeText+"]")
         codeText = $.trim(codeText);
-        // alert("去空格后：["+codeText+"]")
+        // alert("After deleting the space：["+codeText+"]")
 
         if (codeText == null || codeText === "") {
-            //4 提示用户
+            //4 Prompt user
             $("span.errorMsg").text("Fill in the validation code!");
             return false;
         }
-        // 去掉错误信息
+        // Remove error messages
         $("span.errorMsg").text("");
     });
 });
