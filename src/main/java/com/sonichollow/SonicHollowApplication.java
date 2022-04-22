3f16072c79b3c564e30f8d16de6c5251bbade575
Xuening Wang <xuening.wang19@student.xjtlu.edu.cn>
2022-04-22 17:22:37 +0800

    Update SonicHollowApplication.java

--- src/main/java/com/sonichollow/SonicHollowApplication.java
+++ src/main/java/com/sonichollow/SonicHollowApplication.java
@@ -1,50 +1,24 @@
-package com.sonichollow;
+package com.sonichollow.forum;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import java.util.Properties;
-
-import com.google.code.kaptcha.impl.DefaultKaptcha;
-import com.google.code.kaptcha.util.Config;
-
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.client.RestTemplate;
-
 
 @SpringBootApplication
-@MapperScan("com.sonichollow.mapper")
+@ImportResource("classpath:applicationContext.xml")
 @MapperScan(basePackages = "org.sang.mybatis.mapper")
-@ImportResource(locations={"classpath:kaptchaConfig.xml"})
+public class SonicHollowApplication extends SpringBootServletInitializer {
 
-public class SonicHollowApplication {
-
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         SpringApplication.run(SonicHollowApplication.class, args);
     }
 
-    // Google验证码
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SonicHollowApplication.class);
     }
-    @Bean
-    public DefaultKaptcha getDefaultKaptcha(){
-        com.google.code.kaptcha.impl.DefaultKaptcha defaultKaptcha = new com.google.code.kaptcha.impl.DefaultKaptcha();
-        Properties properties = new Properties();
-        properties.setProperty("kaptcha.border", "yes");
-        properties.setProperty("kaptcha.border.color", "105,179,90");
-        properties.setProperty("kaptcha.textproducer.font.color", "blue");
-        properties.setProperty("kaptcha.image.width", "110");
-        properties.setProperty("kaptcha.image.height", "40");
-        properties.setProperty("kaptcha.textproducer.font.size", "30");
-        properties.setProperty("kaptcha.session.key", "code");
-        properties.setProperty("kaptcha.textproducer.char.length", "4");
-        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
-        Config config = new Config(properties);
-        defaultKaptcha.setConfig(config);
-        return defaultKaptcha;
-    }
+
 }
