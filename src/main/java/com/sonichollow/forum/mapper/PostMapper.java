a74bfdf78990d57701e680f947da46e52bb698fe
Xuening Wang <xuening.wang19@student.xjtlu.edu.cn>
2022-04-22 17:27:43 +0800

    Update PostMapper.java

--- src/main/java/com/sonichollow/forum/mapper/PostMapper.java
+++ src/main/java/com/sonichollow/forum/mapper/PostMapper.java
@@ -2,7 +2,21 @@ package com.sonichollow.forum.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sonichollow.forum.entity.Post;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
+@Mapper
 public interface PostMapper extends BaseMapper<Post> {
-}
+    int updateViewCount(@Param("pid") Integer viewCount);
+    Post getAllByPid(@Param("pid") Integer pid);
 
+    Post selectByPid(int pid);
+
+    void deletePost(int pid);
+
+    List<Post> selectAllPost();
+}
