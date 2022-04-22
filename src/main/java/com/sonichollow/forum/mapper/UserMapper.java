44bee30481596909d6d84b254bc8953c03d51272
Xuening Wang <xuening.wang19@student.xjtlu.edu.cn>
2022-04-22 17:28:00 +0800

    Update UserMapper.java

--- src/main/java/com/sonichollow/forum/mapper/UserMapper.java
+++ src/main/java/com/sonichollow/forum/mapper/UserMapper.java
@@ -2,14 +2,21 @@ package com.sonichollow.forum.mapper;
 
 import com.sonichollow.forum.entity.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * 用户模块的持久层接口
  */
+@Component
+@Mapper
 public interface UserMapper {
     /**
      * 插入用户的数据
+     *
      * @param user 用户的数据
      * @return 受影响的行数 （增删改，可以根据返回值判断是否执行成功）
      */
@@ -17,6 +24,7 @@ public interface UserMapper {
 
     /**
      * 根据用户名来查询用户的数据
+     *
      * @param username 用户名
      * @return 如果找到对于的用户，返回其数据，否则返回 null值
      */
@@ -24,16 +32,45 @@ public interface UserMapper {
 
     // Login Function
 
-    @Select("select name from user where name=#{name} and password=#{password}")
-    String isUser(String name, String password);
+    @Select("select username from user where username=#{username} and password=#{password}")
+    String isUser(String username, String password);
 
-    @Select("select name from user where name=#{name}")
-    String checkName(String name);
+    @Select("select username from user where username=#{username}")
+    String checkName(String username);
 
-    @Insert("insert into user(name,email,password) values(#{name},#{email},#{password}) ")
+    @Insert("insert into user(username,email,password) values(#{username},#{email},#{password})")
     Integer addUser(User user);
 
-    @Select("select * from user where name=#{name}")
-    User getUser(String name);
+    @Insert("insert into user(username,email,password,phone,avatar) values(#{username},#{email},#{password},#{phone},#{avatar})")
+    Integer registerUser(User user);
+
+    @Select("select * from user where username=#{username}")
+    User getUser(String username);
+
+    @Select("select * from user where is_delete=0")
+    List<User> findAll();
+
+    // Backstage Managing Function
+
+    List<User> selectAll();
+
+    User selectByUsernameAndPassword(String username, String password);
+
+    void insertUser(String username, String password, int gender, String phone, String email);
+
+    void updateUser(String username, String password, int gender, String phone, String email,int uid);
+
+    void updateUserAvatar(String avatar, int uid);
+
+    User SelectById(int uid);
+
+    void deleteUser(int uid);
+
+
+
 
 }
+
+
+// MyInfo functions
+
